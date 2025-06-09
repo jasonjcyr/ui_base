0ea6987188026f39304ab94861bbb71fff621b84
Jason Cyr <jasonjcyr@hotmail.com>
2025-06-09 14:01:13 -0400

    publish fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,26 +6,13 @@ import cssInjected from 'vite-plugin-css-injected-by-js';
 import dts from 'vite-plugin-dts';
 import { externalizeDeps } from 'vite-plugin-externalize-deps';
 
-const isCI = false; // process.env.CI === 'true';
-
-let visualizer;
-try {
-  // Only require if not in CI and the package is installed
-  if (!isCI) {
-    visualizer = require('rollup-plugin-visualizer').visualizer;
-  }
-} catch {
-  visualizer = () => null;
-}
-
 export default defineConfig({
   plugins: [
     react(),
     dts({ insertTypesEntry: true }),
     externalizeDeps({ except: [] }),
     cssInjected(),
-    !isCI && visualizer({ open: false }),
-  ].filter(Boolean),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
